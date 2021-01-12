57d68a8aa3852ca3a9e220290410a61082ebeeaa
yoel.lebrech@gmail.com <yoel.lebrech@gmail.com>
2021-01-12 16:57:03 +0100

    push du 12/01

--- Vue/page.php
+++ Vue/page.php
@@ -4,6 +4,7 @@
 <title>Accueil</title>
 <link rel="stylesheet" href="css/bootstrap.css" type="text/css" />
 <link rel="stylesheet" href="css/style.css" type="text/css" />
+<script src="js/bootstrap.js"> </script>
 </head>
 
 <header>
@@ -17,10 +18,22 @@
 
 
 
-	<div class="corps" style="margin-top: 5%;" ;>
+	<div class="corps centrer" style="margin-top: 5%;"  ;>
 
 	<p class=" centrer">Bienvenue, sur la page d'accueil! <br/> Merci d'utiliser notre service de technologie 2.0 ! 
 	</p>
+
+	<div class="dropdown">
+  <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    Dropdown link
+  </a>
+
+  <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+    <a class="dropdown-item" href="#">Action</a>
+    <a class="dropdown-item" href="#">Another action</a>
+    <a class="dropdown-item" href="#">Something else here</a>
+  </div>
+</div>
 
 	</div>
 	
