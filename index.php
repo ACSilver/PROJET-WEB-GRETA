913773c6cc5e1fd22b933ebc0ffb880ed7d67e67
azalonga <38530061+azalonga@users.noreply.github.com>
2021-01-12 10:08:11 +0100

    page index (login) faire

--- index.php
+++ index.php
@@ -1,1 +1,38 @@
-hello
+<?php  
+
+//session_start();
+//session_destroy();
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Mon site</title>
+<link rel="stylesheet" href="css/style.css" type="text/css" />
+</head>
+<body>
+	<div class="corps">
+		<p>
+		<div class="contenu">
+			<h3>Page d'introduction</h3>
+			<p>
+			<form method="post" action="page.php?article=intro"   >
+					<p>
+						<label>Votre identifiant :</label>
+						<input type="text" name="id"   >
+					</p>
+					<p>
+						<label>Mot de passe :</label>
+						<input type="password" name="mot_de_passe"  >
+					</p>
+					<p>
+						<input type="submit" value="Valider">
+					</p>
+			</form>
+			</p>
+		</div>
+		</p>
+	</div>
+</body>
+</html>
