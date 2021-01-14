62ef5ddd375f6d80187e894e683379701bbc91d1
azalonga <38530061+azalonga@users.noreply.github.com>
2021-01-14 09:37:24 +0100

    waseem

--- Vue/header.php
+++ Vue/header.php
@@ -1,1 +1,94 @@
-test
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Header</title>
+        <link rel="stylesheet" href="../css/bootstrap.css" type="text/css" />
+        <link rel="stylesheet" href="../css/style.css" />
+    </head>
+    <body class="truc"> <!-- Header pour le formateur  -->
+    
+    <!-- <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Accueil</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">GRETA</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Formations</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Stagiaires</a>
+        </li>
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Recherche" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Recherche</button>
+      </form>
+    </div>
+  </div>
+</nav>
+
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark"> Header pour le stagiaire  
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Accueil</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">GRETA</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Formations</a>
+        </li>
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Recherche" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Recherche</button>
+      </form>
+    </div>
+  </div>
+</nav>
+
+-->
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark"> <!-- Header pour l'admin  -->
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Accueil</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">GRETA</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Formations</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Formateurs</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Stagiaires</a>
+        </li>
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Recherche" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Recherche</button>
+      </form>
+    </div>
+  </div>
+</nav>
+
+
+    </body>
+
+</html>
