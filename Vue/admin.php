b3555bbc3501db292e6e6089f7ceb8d319437442
yoel.lebrech@gmail.com <yoel.lebrech@gmail.com>
2021-01-14 16:12:23 +0100

    footer

--- Vue/admin.php
+++ Vue/admin.php
@@ -14,103 +14,179 @@
 </header>
 
 
-<body class="centrer fond "   > 
+<body class="centrer fond "    > 
 
 
 
-	<div class="corps centrer" style="margin-top: 5%; ;"  ;>
+	<div class="corps centrer" >
 
 	    <p class=" centrer">Bienvenue, sur la page d'accueil! <br/> Merci d'utiliser notre service de technologie 2.0 ! </p>
 
         <table class="table">
-        <thead>
-          <tr>
-            <th>Formations</th>
-            <th>Année de la promotion</th>
+            <thead>
+                <tr>
+                    <th>Formations</th>
+                    <th>Année de la promotion</th>
             
-          </tr>
-        </thead>
+                </tr>
+            </thead>
         <tbody>
-          <tr>
-            <td><br><a href="btssio.php">BTS SIO</a></td>
-            <td><div class="form-group">
-  <label for="sel1"></label>
-  <select class="form-control" id="sel1">
-    <option>2020-2021</option>
-    <option>2019-2020</option>
-    <option>2018-2019</option>
-    <option>2017-2018</option>
-  </select>
-</div></td>
+            <tr>
+                <td><br><a href="btssio.php">BTS SIO</a></td>
+                <td>
+                    <div class="form-group">
+                    <label for="sel1"></label>
+                    <select class="form-control" id="sel1">
+                        <option>2020-2021</option>
+                        <option>2019-2020</option>
+                        <option>2018-2019</option>
+                        <option>2017-2018</option>
+                    </select> 
+                    </div>
+                </td>
           </tr>
           <tr>
-            <td>BTS MCO</td>
-            <td><div class="form-group">
-  <label for="sel1"></label>
-  <select class="form-control" id="sel1">
-    <option>2020-2021</option>
-    <option>2019-2020</option>
-    <option>2018-2019</option>
-    <option>2017-2018</option>
-  </select>
-</div></td>
+                <td>BTS MCO</td>
+                <td>
+                    <div class="form-group">
+                        <label for="sel1"></label>
+                        <select class="form-control" id="sel1">
+                            <option>2020-2021</option>
+                            <option>2019-2020</option>
+                            <option>2018-2019</option>
+                            <option>2017-2018</option>
+                        </select>
+                    </div>
+                </td>
            
           </tr>
           <tr>
-            <td>BTS MUC</td>
-            <td><div class="form-group">
-  <label for="sel1"></label>
-  <select class="form-control" id="sel1">
-    <option>2020-2021</option>
-    <option>2019-2020</option>
-    <option>2018-2019</option>
-    <option>2017-2018</option>
-  </select>
-</div></td>
-
-
-            
+                <td>BTS MUC</td>
+                <td>
+                    <div class="form-group">
+                        <label for="sel1"></label>
+                        <select class="form-control" id="sel1">
+                            <option>2020-2021</option>
+                            <option>2019-2020</option>
+                            <option>2018-2019</option>
+                            <option>2017-2018</option>
+                        </select>
+                    </div>
+                </td>
           </tr>
+
           <tr>
-            <td>BTS MUC</td>
-            <td><div class="form-group">
-  <label for="sel1"></label>
-  <select class="form-control" id="sel1">
-    <option>2020-2021</option>
-    <option>2019-2020</option>
-    <option>2018-2019</option>
-    <option>2017-2018</option>
-  </select>
-</div></td>
-
-
-            
+                <td>BTS MUC</td>
+                <td>
+                    <div class="form-group">
+                        <label for="sel1"></label>
+                        <select class="form-control" id="sel1">
+                            <option>2020-2021</option>
+                            <option>2019-2020</option>
+                            <option>2018-2019</option>
+                            <option>2017-2018</option>
+                        </select>
+                    </div>
+                </td>
           </tr>
+
           <tr>
-            <td>BTS MUC</td>
-            <td><div class="form-group">
-  <label for="sel1"></label>
-  <select class="form-control" id="sel1">
-    <option>2020-2021</option>
-    <option>2019-2020</option>
-    <option>2018-2019</option>
-    <option>2017-2018</option>
-  </select>
-</div></td>
-
-
-            
+                <td>BTS MUC</td>
+                <td>
+                    <div class="form-group">
+                        <label for="sel1"></label>
+                        <select class="form-control" id="sel1">
+                            <option>2020-2021</option>
+                            <option>2019-2020</option>
+                            <option>2018-2019</option>
+                            <option>2017-2018</option>
+                        </select>
+                    </div>
+                </td>
           </tr>
-        </tbody>
-      </table>
 
+          <tr>
+                <td>BTS MUC</td>
+                <td>
+                    <div class="form-group">
+                        <label for="sel1"></label>
+                        <select class="form-control" id="sel1">
+                            <option>2020-2021</option>
+                            <option>2019-2020</option>
+                            <option>2018-2019</option>
+                            <option>2017-2018</option>
+                        </select>
+                    </div>
+                </td>
+          </tr>
 
+          <tr>
+                <td>BTS MUC</td>
+                <td>
+                    <div class="form-group">
+                        <label for="sel1"></label>
+                        <select class="form-control" id="sel1">
+                            <option>2020-2021</option>
+                            <option>2019-2020</option>
+                            <option>2018-2019</option>
+                            <option>2017-2018</option>
+                        </select>
+                    </div>
+                </td>
+          </tr>
 
-	</div>
-	
+          <tr>
+                <td>BTS MUC</td>
+                <td>
+                    <div class="form-group">
+                        <label for="sel1"></label>
+                        <select class="form-control" id="sel1">
+                            <option>2020-2021</option>
+                            <option>2019-2020</option>
+                            <option>2018-2019</option>
+                            <option>2017-2018</option>
+                        </select>
+                    </div>
+                </td>
+          </tr>
+
+          <tr>
+                <td>BTS MUC</td>
+                <td>
+                    <div class="form-group">
+                        <label for="sel1"></label>
+                        <select class="form-control" id="sel1">
+                            <option>2020-2021</option>
+                            <option>2019-2020</option>
+                            <option>2018-2019</option>
+                            <option>2017-2018</option>
+                        </select>
+                    </div>
+                </td>
+          </tr>
+
+          <tr>
+                <td>BTS MUC</td>
+                <td>
+                    <div class="form-group">
+                        <label for="sel1"></label>
+                        <select class="form-control" id="sel1">
+                            <option>2020-2021</option>
+                            <option>2019-2020</option>
+                            <option>2018-2019</option>
+                            <option>2017-2018</option>
+                        </select>
+                    </div>
+                </td>
+          </tr>
+
+    </div>
+    
 </body>
-<footer class="">
-<?php include("footer.php"); ?>
+<footer class="footers" >
+
+    <?php include("footer.php"); ?>
+
 </footer>
 </html>
 
--- Vue/footer.php
+++ Vue/footer.php
@@ -2,31 +2,16 @@
 <head>
 <title>Mon site</title>
 <link rel="stylesheet" href="../css/bootstrap.css" type="text/css" />
-        <link rel="stylesheet" href="../css/style.css" />
+<link rel="stylesheet" href="../css/style.css" />
 </head>
-  <footer class="site-footer  " >
-      <div class="container">
-        <div class="row">
-          <div class="col-sm-12 col-md-6">
-            <h6>A propos</h6>
-            <p class="text-justify">Application Web qui permet de remplir l'attestation de compétence de manière simple. </p>
-          </div>
+<body>
 
-
-          </div>
-        </div>
-        <hr>
-      </div>
-      <div class="container">
-        <div class="row">
-          <div class="col-md-8 col-sm-6 col-xs-12">
-            <p class="copyright-text">Application Web développée par Aymeric, Wasseem, Julien et Gwenc'hlan.
-            </p>
-          </div>
-
-
-        </div>
-      </div>
+</body>
+<footer  >
+  <div >
+    <h6>A propos</h6>
+    <p class="text-justify">Application Web qui permet de remplir l'attestation de compétence de manière simple. </p>
+    <p class="copyright-text">Application Web développée par Aymeric, Wasseem, Julien et Gwenc'hlan.</p>
+  </div>
 </footer>
-
 </html>
