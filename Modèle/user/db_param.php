ccb230af9ae9fa73c02a0685557321ab1000e361
Aymeric.C <Aymcormier@gmail.com>
2021-01-15 14:02:43 +0100

    suppression fichiers inutiles

--- Modèle/user/db_param.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-/*
-*  Paramètres de la table USER
-*  Inclus dans tous les fichiers du dossier : controler/user
-*/
-
-define('DB_USER_TABLE', 'clients_tbl'); // nom de la table d'utilisateur
-
