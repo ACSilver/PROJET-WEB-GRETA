b1fecfd7cf0157b39e9904e13efbe2f10fc19c10
yoel.lebrech@gmail.com <yoel.lebrech@gmail.com>
2021-05-21 11:11:33 +0200

    Page stagiaire

--- Vue/admin/listepromoadmin.php
+++ Vue/admin/listepromoadmin.php
@@ -64,7 +64,7 @@ $promos = $query2->fetchAll();
                 <tbody>
                     <?php 
                         foreach($promos as $key => $value) {
-                            echo '<tr><td><br> <a href="test" >'.$value['promo'].'</a></td>'; 
+                            echo '<tr><td><br> <a href="Stagiaires" >'.$value['promo'].'</a></td>'; 
                             echo '</tr>';
                         }
                     ?>
